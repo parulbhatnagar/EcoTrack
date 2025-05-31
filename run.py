33b65dc4368a1f4e7f97146c1d82c426b8353824
Shweta Gahlawat <90508189+shwetagahlawat@users.noreply.github.com>
2025-05-31 13:08:52 +0530

    Add files via upload

---
+++ run.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from dailyinput.routes import dailyinput_bp
+
+app = Flask(__name__)
+app.register_blueprint(dailyinput_bp)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5002)
+
+
+
